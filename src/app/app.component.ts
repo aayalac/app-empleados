86b932d515b7388b1170aa7a7e0a93d44e6a5b29
Miguel Angel Ayala <ingeniero.ayala@outlook.com>
2024-02-14 12:27:14 -0500

    feat: se crea formulario para registrar empleados (utilizacion de Directivas)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { Empleado } from './empleado.model';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app-empleados';
+  titulo = 'Listado De Empleados';
+
+  empleados:Empleado[]=[
+
+    new Empleado("Juan","Gomez","Presidente",7800),
+    new Empleado("Jose","SIlva","Jefe",4800),
+    new Empleado("Leo","Dan","Administrativo",2500),
+    new Empleado("Antonia","Vélez","Supervisora",3000),
+  ];
+
+  agregarEmpleado(){
+
+    let miEmpleado=new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario)
+    this.empleados.push(miEmpleado)
+  }
+
+  cuadroNombre:string="";
+  cuadroApellido:string="";
+  cuadroCargo:string="";
+  cuadroSalario:number=0;
 }
